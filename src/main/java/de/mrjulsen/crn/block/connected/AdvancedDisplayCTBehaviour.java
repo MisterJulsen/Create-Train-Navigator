b8efc0c0c1f442eee75d6ee526ba752504b5f4c9
MisterJulsen <geisterdrache02@gmail.com>
2024-03-24 11:13:09 +0100

    Updated visual connection code

--- src/main/java/de/mrjulsen/crn/block/connected/AdvancedDisplayCTBehaviour.java
+++ src/main/java/de/mrjulsen/crn/block/connected/AdvancedDisplayCTBehaviour.java
@@ -3,13 +3,11 @@ package de.mrjulsen.crn.block.connected;
 import com.simibubi.create.foundation.block.connected.CTSpriteShiftEntry;
 import com.simibubi.create.foundation.block.connected.ConnectedTextureBehaviour;
 
-import de.mrjulsen.crn.block.AbstractAdvancedDisplayBlock;
 import de.mrjulsen.crn.block.be.AdvancedDisplayBlockEntity;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class AdvancedDisplayCTBehaviour extends ConnectedTextureBehaviour.Base {
@@ -26,30 +24,8 @@ public class AdvancedDisplayCTBehaviour extends ConnectedTextureBehaviour.Base {
 	}
 
     @Override
-	public boolean connectsTo(BlockState state, BlockState other, BlockAndTintGetter reader, BlockPos pos, BlockPos otherPos, Direction face, Direction primaryOffset, Direction secondaryOffset) {
-		if (state.getBlock() instanceof AbstractAdvancedDisplayBlock block &&
-		    reader.getBlockEntity(pos) instanceof AdvancedDisplayBlockEntity blockEntity &&
-			reader.getBlockEntity(otherPos) instanceof AdvancedDisplayBlockEntity otherBlockEntity
-		) {
-			if (other.getBlock() != state.getBlock()) {
-				return false;
-			}
-			if (other.getValue(HorizontalDirectionalBlock.FACING) != state.getValue(HorizontalDirectionalBlock.FACING)) {
-				return false;
-			}
-			if (other.getValue(AbstractAdvancedDisplayBlock.SIDE) != state.getValue(AbstractAdvancedDisplayBlock.SIDE)) {
-				return false;
-			}
-			if (pos.below().equals(otherPos) && (!state.getValue(AbstractAdvancedDisplayBlock.DOWN) || block.isSingleLine(state, blockEntity) || !blockEntity.isDisplayCompatible(otherBlockEntity))) {
-				return false;
-			}
-			if (pos.above().equals(otherPos) && (!state.getValue(AbstractAdvancedDisplayBlock.UP) || block.isSingleLine(state, blockEntity) || !blockEntity.isDisplayCompatible(otherBlockEntity))) {
-				return false;
-			}
-				
-			return true;
-		}
-		return false;
+	public boolean connectsTo(BlockState state, BlockState other, BlockAndTintGetter reader, BlockPos pos, BlockPos otherPos, Direction face, Direction primaryOffset, Direction secondaryOffset) {		
+		return reader.getBlockEntity(pos) instanceof AdvancedDisplayBlockEntity blockEntity && blockEntity.connectable(reader, pos, otherPos);
 	}
 
 }
