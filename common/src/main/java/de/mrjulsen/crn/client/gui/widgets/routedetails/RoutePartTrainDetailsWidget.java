25218c4988b5a854780c3123cdc3c2800b9989a0
MrJulsen <geisterdrache02@gmail.com>
2025-01-13 21:47:53 +0100

    Fixed: Train Name is not shown in the route details

--- common/src/main/java/de/mrjulsen/crn/client/gui/widgets/routedetails/RoutePartTrainDetailsWidget.java
+++ common/src/main/java/de/mrjulsen/crn/client/gui/widgets/routedetails/RoutePartTrainDetailsWidget.java
@@ -126,7 +126,7 @@ public class RoutePartTrainDetailsWidget extends DLWidgetContainer implements Cl
         graphics.poseStack().scale(scale, scale, scale);
         Component trainName = TextUtils.text(part.getFirstStop().getTrainDisplayName()).withStyle(ChatFormatting.BOLD);
         CreateDynamicWidgets.renderTextHighlighted(graphics, (int)((x() + 80 + 24) / scale), (int)((y + 4) / scale), font, trainName, part.getFirstStop().getTrainDisplayColor());
-        GuiUtils.drawString(graphics, font, (int)((x() + 80 + 24) / scale) + font.width(trainName) + 10, (int)((y + 6) / scale), GuiUtils.ellipsisString(font, TextUtils.text(String.format("%s (%s)", stop.getTrainDisplayName(), stop.getTrainId().toString().split("-")[0])), (int)((maxWidth - font.width(trainName) - 15) / scale)), 0xFFDBDBDB, EAlignment.LEFT, false);
+        GuiUtils.drawString(graphics, font, (int)((x() + 80 + 24) / scale) + font.width(trainName) + 10, (int)((y + 6) / scale), GuiUtils.ellipsisString(font, TextUtils.text(String.format("%s (%s)", stop.getTrainName(), stop.getTrainId().toString().split("-")[0])), (int)((maxWidth - font.width(trainName) - 25) / scale)), 0xFFDBDBDB, EAlignment.LEFT, false);
         GuiUtils.drawString(graphics, font, (int)((x() + 80 + 24) / scale), (int)((y + 18) / scale), GuiUtils.ellipsisString(font, TextUtils.text(stop.getDisplayTitle()), (int)((maxWidth - 24) / scale)), 0xFFDBDBDB, EAlignment.LEFT, false);        
         graphics.poseStack().scale(mul, mul, mul);
         graphics.poseStack().popPose();
