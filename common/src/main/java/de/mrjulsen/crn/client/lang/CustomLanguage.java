47d239f69996fc25853c281a3c52ac3718dca6a7
MrJulsen <geisterdrache02@gmail.com>
2025-01-13 22:14:43 +0100

    Updated translations

--- common/src/main/java/de/mrjulsen/crn/client/lang/CustomLanguage.java
+++ common/src/main/java/de/mrjulsen/crn/client/lang/CustomLanguage.java
@@ -25,8 +25,13 @@ public enum CustomLanguage implements StringRepresentable {
     BASQUE("basque", "eu_es"),
     ITALIAN("italian", "it_it"),
     JAPANESE("japanese", "ja_jp"),
-    PORTUGUESE_brazilian("portuguese_brazilian", "pt_br"),
-    UKRAINIAN("ukrainian", "uk_ua");
+    PORTUGUESE_BRAZILIAN("portuguese_brazilian", "pt_br"),
+    UKRAINIAN("ukrainian", "uk_ua"),
+    ARABIC("arabic", "ar_sa"),
+    CZECH("czech", "cs_cz"),
+    GERMAN_SWITZERLAND("german_switzerland", "de_ch"),
+    ROMANIAN("romanian", "ro_ro"),
+    TURKISH("turkish", "tr_tr");
 
     private String name;
     private String code;
