36994ccd4414eb3dd67e2e506cb734f81cf97c28
MrJulsen <geisterdrache02@gmail.com>
2025-04-03 12:13:53 +0200

    Fixed crash

--- common/src/main/java/de/mrjulsen/crn/client/gui/widgets/DLNewListBox.java
+++ common/src/main/java/de/mrjulsen/crn/client/gui/widgets/DLNewListBox.java
@@ -118,7 +118,7 @@ public class DLNewListBox<T, W extends DLNewListBox.Entry<T, W>> extends DLScrol
         if (draggedEntry != null && currentData != null) {
             int oldIndex = entryWidgets.indexOf(draggedEntry);
             int newIndex = dropIndex >= 0 ? dropIndex : oldIndex;
-            if (oldIndex > 0 && newIndex > 0 && oldIndex < entryWidgets.size() || newIndex < entryWidgets.size()) {                
+            if (oldIndex > 0 && newIndex > 0 && oldIndex < entryWidgets.size() && newIndex < entryWidgets.size()) {                
                 T element = currentData.remove(oldIndex);
                 currentData.add(newIndex, element);
             }
