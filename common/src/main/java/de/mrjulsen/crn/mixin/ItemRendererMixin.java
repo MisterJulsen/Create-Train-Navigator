ac10033ee417f133aaf4ba3f1ff50dc32b1dc8e7
MisterJulsen <julian.ramsperger@t-online.de>
2025-04-22 13:12:18 +0200

    Updated navigator item renderer

--- common/src/main/java/de/mrjulsen/crn/mixin/ItemRendererMixin.java
+++ common/src/main/java/de/mrjulsen/crn/mixin/ItemRendererMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import com.mojang.blaze3d.vertex.PoseStack;
 
-import de.mrjulsen.crn.item.NavigatorItem;
+import de.mrjulsen.crn.client.ClientWrapper;
 import de.mrjulsen.crn.registry.ModItems;
 import net.minecraft.client.renderer.ItemModelShaper;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -25,7 +25,7 @@ public abstract class ItemRendererMixin {
     @ModifyVariable(method = "render", at = @At(value = "HEAD"), argsOnly = true)
     public BakedModel onRender(BakedModel value, ItemStack itemStack, ItemTransforms.TransformType transformType, boolean leftHand, PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay, BakedModel model) {
         if (itemStack.is(ModItems.NAVIGATOR.get()) && (transformType != TransformType.FIRST_PERSON_RIGHT_HAND && transformType != TransformType.FIRST_PERSON_LEFT_HAND)) {
-            return itemModelShaper.getModelManager().getModel(NavigatorItem.WORLD_MODEL);
+            return itemModelShaper.getModelManager().getModel(ClientWrapper.NAVIGATOR_WORLD_MODEL);
         }
         return value;
     }
--- common/src/main/java/de/mrjulsen/crn/mixin/ModelBakeryMixin.java
+++ common/src/main/java/de/mrjulsen/crn/mixin/ModelBakeryMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import de.mrjulsen.crn.item.NavigatorItem;
+import de.mrjulsen.crn.client.ClientWrapper;
 import dev.architectury.injectables.annotations.PlatformOnly;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.resources.model.ModelBakery;
@@ -22,12 +22,12 @@ public class ModelBakeryMixin {
     @PlatformOnly(value = PlatformOnly.FABRIC)
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/ModelBakery;loadTopLevel(Lnet/minecraft/client/resources/model/ModelResourceLocation;)V", ordinal = 0, shift = At.Shift.AFTER))
     public void registerCustomModels(ResourceManager resourceManager, BlockColors blockColors, ProfilerFiller profiler, int maxMipmapLevel, CallbackInfo ci) {
-        this.loadTopLevel(NavigatorItem.WORLD_MODEL);
+        this.loadTopLevel(ClientWrapper.NAVIGATOR_WORLD_MODEL);
     }
     
     @PlatformOnly(value = PlatformOnly.FORGE)
     @Inject(method = "processLoading", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/ModelBakery;loadTopLevel(Lnet/minecraft/client/resources/model/ModelResourceLocation;)V", ordinal = 0, shift = At.Shift.AFTER))
     public void registerCustomModels(ProfilerFiller profiler, int b, CallbackInfo ci) {
-        this.loadTopLevel(NavigatorItem.WORLD_MODEL);
+        this.loadTopLevel(ClientWrapper.NAVIGATOR_WORLD_MODEL);
     }
 }
