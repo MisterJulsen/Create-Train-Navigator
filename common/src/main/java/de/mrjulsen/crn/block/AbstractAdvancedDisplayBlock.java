fe31dbf91ee11649f93c8770250484b7f5c19905
MrJulsen <geisterdrache02@gmail.com>
2025-05-15 10:10:30 +0200

    Fixed crash when using Clipboard or Nametag on a display

--- common/src/main/java/de/mrjulsen/crn/block/AbstractAdvancedDisplayBlock.java
+++ common/src/main/java/de/mrjulsen/crn/block/AbstractAdvancedDisplayBlock.java
@@ -374,8 +374,12 @@ public abstract class AbstractAdvancedDisplayBlock extends Block implements IWre
 			AdvancedDisplayBlockEntity controller = blockEntity.getController();
             if (controller != null) {
 				SimpleStaticTextDisplaySettings settings = new SimpleStaticTextDisplaySettings();				
-				settings.setStaticText(heldItem.getHoverName().getString());	
-				CreateRailwaysNavigator.net().CHANNEL.sendToServer(new AdvancedDisplayUpdatePacket(controller.getLevel(), controller.getBlockPos(), ModDisplayTypes.SIMPLE_TEXT, controller.getBlockState().getValue(AbstractAdvancedSidedDisplayBlock.SIDE) == ESide.BOTH, settings));
+				settings.setStaticText(heldItem.getHoverName().getString());
+				boolean doubleSided = false;
+				if (controller.getBlockState().getBlock() instanceof AbstractAdvancedSidedDisplayBlock) {
+					doubleSided = controller.getBlockState().getValue(AbstractAdvancedSidedDisplayBlock.SIDE) == ESide.BOTH;
+				}
+				CreateRailwaysNavigator.net().CHANNEL.sendToServer(new AdvancedDisplayUpdatePacket(controller.getLevel(), controller.getBlockPos(), ModDisplayTypes.SIMPLE_TEXT, doubleSided, settings));
 				return InteractionResult.SUCCESS;
             }
 		} else if (AllBlocks.CLIPBOARD.isIn(heldItem) && pLevel.isClientSide) {
@@ -403,7 +407,11 @@ public abstract class AbstractAdvancedDisplayBlock extends Block implements IWre
 						}
 					}
 				}
-				CreateRailwaysNavigator.net().CHANNEL.sendToServer(new AdvancedDisplayUpdatePacket(controller.getLevel(), controller.getBlockPos(), ModDisplayTypes.RICH_TEXT, controller.getBlockState().getValue(AbstractAdvancedSidedDisplayBlock.SIDE) == ESide.BOTH, settings));
+				boolean doubleSided = false;
+				if (controller.getBlockState().getBlock() instanceof AbstractAdvancedSidedDisplayBlock) {
+					doubleSided = controller.getBlockState().getValue(AbstractAdvancedSidedDisplayBlock.SIDE) == ESide.BOTH;
+				}
+				CreateRailwaysNavigator.net().CHANNEL.sendToServer(new AdvancedDisplayUpdatePacket(controller.getLevel(), controller.getBlockPos(), ModDisplayTypes.RICH_TEXT, doubleSided, settings));
 				return InteractionResult.SUCCESS;
             }
 		}
