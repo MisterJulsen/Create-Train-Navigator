1beb3c0825177a13d838c1ad2934d730062e2663
MrJulsen <geisterdrache02@gmail.com>
2025-06-02 14:54:22 +0200

    Fixed random errors on server

--- common/src/main/java/de/mrjulsen/crn/block/display/AdvancedDisplayTarget.java
+++ common/src/main/java/de/mrjulsen/crn/block/display/AdvancedDisplayTarget.java
@@ -117,6 +117,8 @@ public class AdvancedDisplayTarget extends DisplayTarget {
 						}
 					}
 
+					if (controller.getDisplayType().category().getSource() != EDisplayTypeDataSource.PLATFORM) return;
+
 					List<StationDisplayData> preds = prepare(filter, controller.getDisplayProperties().platformDisplayTrainsCount().apply(controller));
 					controller.setData(
 							preds,
@@ -129,6 +131,8 @@ public class AdvancedDisplayTarget extends DisplayTarget {
 					if (controller.getDisplayType() != ModDisplayTypes.RICH_TEXT) {
 						if (!ModCommonConfig.AUTO_UPDATE_DISPLAY_TYPE.get()) return;
 					}
+
+					if (controller.getDisplayType() != ModDisplayTypes.RICH_TEXT) return;
 
 					StaticTextDisplaySettings settings = controller.getSettingsAs(StaticTextDisplaySettings.class).orElse(new StaticTextDisplaySettings());
 					int currentLine = line;
