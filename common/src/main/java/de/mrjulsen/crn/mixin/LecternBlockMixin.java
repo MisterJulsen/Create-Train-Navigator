4bef1a5327d7a098a7ab2e0deb241f016fbb4627
MrJulsen <geisterdrache02@gmail.com>
2025-06-03 08:57:40 +0200

    Fixed: Books can no longer be placed on lecterns

--- common/src/main/java/de/mrjulsen/crn/mixin/LecternBlockMixin.java
+++ common/src/main/java/de/mrjulsen/crn/mixin/LecternBlockMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.mrjulsen.crn.registry.ModBlocks;
+import de.mrjulsen.crn.registry.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
@@ -22,7 +23,7 @@ public class LecternBlockMixin {
     
     @Inject(method = "useItemOn", at = @At(value = "HEAD"), cancellable = true)
     public void useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<ItemInteractionResult> cir) {
-        if (state.is(Blocks.LECTERN) && !state.getValue(LecternBlock.HAS_BOOK)) {
+        if (stack.is(ModItems.NAVIGATOR.getDelegate()) && state.is(Blocks.LECTERN) && !state.getValue(LecternBlock.HAS_BOOK)) {
             if (!level.isClientSide) {
                 ItemStack lecternStack = player.isCreative() ? stack.copy() : stack.split(1);
                 ModBlocks.NAVIGATOR_LECTERN.get().replaceLectern(state, level, pos, lecternStack);
