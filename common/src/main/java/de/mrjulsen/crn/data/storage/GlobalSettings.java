ae12090d6713e8ed5939cac665ea632b98196384
MrJulsen <geisterdrache02@gmail.com>
2025-06-21 13:09:09 +0200

    Fixed concurrent modification exception

--- common/src/main/java/de/mrjulsen/crn/data/storage/GlobalSettings.java
+++ common/src/main/java/de/mrjulsen/crn/data/storage/GlobalSettings.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
 
@@ -13,8 +15,6 @@ import com.simibubi.create.content.trains.entity.Train;
 import com.simibubi.create.content.trains.station.GlobalStation;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
@@ -63,11 +63,11 @@ public class GlobalSettings implements INBTSerializable {
 
     private final MinecraftServer server;
 
-    private final Map<UUID, StationTag> stationTags = new HashMap<>();
-    private final Map<UUID, TrainCategory> trainCategories = new HashMap<>();
-    private final Map<UUID, TrainLine> trainLines = new HashMap<>();
-    private final Set<String> stationBlacklist = new HashSet<>();
-    private final Set<String> trainBlacklist = new HashSet<>();
+    private final Map<UUID, StationTag> stationTags = new ConcurrentHashMap<>();
+    private final Map<UUID, TrainCategory> trainCategories = new ConcurrentHashMap<>();
+    private final Map<UUID, TrainLine> trainLines = new ConcurrentHashMap<>();
+    private final Set<String> stationBlacklist = new ConcurrentSkipListSet<>();
+    private final Set<String> trainBlacklist = new ConcurrentSkipListSet<>();
 
     private static GlobalSettings instance;
 
