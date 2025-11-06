1bb4570b6878d9d8ae01c3c2a46b04da3f65f042
MrJulsen <geisterdrache02@gmail.com>
2025-11-06 23:46:54 +0100

    Updated gui component

--- common/src/main/java/de/mrjulsen/crn/client/gui/widgets/FlatCheckBox.java
+++ common/src/main/java/de/mrjulsen/crn/client/gui/widgets/FlatCheckBox.java
@@ -1,34 +1,34 @@
 package de.mrjulsen.crn.client.gui.widgets;
 
-import java.util.function.Consumer;
-
 import de.mrjulsen.mcdragonlib.DragonLib;
-import de.mrjulsen.mcdragonlib.client.gui.widgets.DLCheckBox;
+import de.mrjulsen.mcdragonlib.client.gui.widgets.components.DLCheckBox;
 import de.mrjulsen.mcdragonlib.client.render.GuiIcons;
-import de.mrjulsen.mcdragonlib.client.util.Graphics;
+import de.mrjulsen.mcdragonlib.client.util.DLGuiGraphics;
 import de.mrjulsen.mcdragonlib.client.util.GuiUtils;
-import de.mrjulsen.mcdragonlib.core.ETextAlignment;
+import de.mrjulsen.mcdragonlib.data.ETextAlignment;
+import de.mrjulsen.mcdragonlib.util.DLColor;
+import de.mrjulsen.mcdragonlib.util.math.Rectangle;
 
 public class FlatCheckBox extends DLCheckBox {
-
-    public FlatCheckBox(int pX, int pY, int pWidth, String pMessage, boolean checked, Consumer<DLCheckBox> onCheckedChanged) {
-        super(pX, pY, pWidth, pMessage, checked, onCheckedChanged);
-        set_height(GuiIcons.ICON_SIZE);
-    }
     
-    @Override
-    public void renderMainLayer(Graphics graphics, int pMouseX, int pMouseY, float pPartialTick) {
-        if (isChecked()) {
-            GuiIcons.CHECKMARK.render(graphics, x(), y());
-        }
-        GuiUtils.drawString(graphics, font, x() + GuiIcons.ICON_SIZE + 2, y() + height() / 2 - font.lineHeight / 2, getMessage(), DragonLib.NATIVE_BUTTON_FONT_COLOR_ACTIVE, ETextAlignment.LEFT, false);
-
-        if (isMouseSelected()) {
-            GuiUtils.fill(graphics, x(), y(), width(), height(), 0x44FFFFFF);
-        }
+    public FlatCheckBox(int x, int y, int w, int h) {
+        super(x, y, w, h);
     }
 
     @Override
-    public void renderFrontLayer(Graphics graphics, int mouseX, int mouseY, float partialTicks) {
+    public void setHeight(double height) {        
+        super.setHeight(GuiIcons.ICON_SIZE);
+    }
+
+    @Override
+    public void renderMainLayer(DLGuiGraphics graphics, double mouseX, double mouseY, Rectangle renderBounds) {
+        if (checked.get()) {
+            GuiIcons.CHECKMARK.render(graphics, 0, 0);
+        }
+        GuiUtils.drawString(graphics, graphics.defaultFont(), GuiIcons.ICON_SIZE + 2, height() / 2 - graphics.defaultFont().lineHeight / 2, text.get(), DragonLib.VANILLA_BUTTON_ACTIVE_FONT_COLOR, ETextAlignment.LEFT, false);
+
+        if (isSelected()) {
+            GuiUtils.fill(graphics, 0, 0, width(), height(), DLColor.fromInt(0x44FFFFFF));
+        }
     }
 }
