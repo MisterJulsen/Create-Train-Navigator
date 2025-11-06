b0d61aa8442ad989d865ab3ff05cca97059f4909
MrJulsen <geisterdrache02@gmail.com>
2025-11-06 23:44:38 +0100

    Updated gui component

--- common/src/main/java/de/mrjulsen/crn/client/gui/widgets/DLCreateTextBox.java
+++ common/src/main/java/de/mrjulsen/crn/client/gui/widgets/DLCreateTextBox.java
@@ -1,20 +1,23 @@
 package de.mrjulsen.crn.client.gui.widgets;
 
 import de.mrjulsen.crn.client.gui.CreateDynamicWidgets;
-import de.mrjulsen.mcdragonlib.client.gui.widgets.DLEditBox;
-import de.mrjulsen.mcdragonlib.client.util.Graphics;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.network.chat.Component;
+import de.mrjulsen.crn.client.gui.widgets.renderers.CreateTextBoxRenderer;
+import de.mrjulsen.mcdragonlib.client.gui.widgets.components.DLRichTextEditBox;
+import de.mrjulsen.mcdragonlib.client.util.DLGuiGraphics;
+import de.mrjulsen.mcdragonlib.util.math.Rectangle;
+import de.mrjulsen.mcdragonlib.util.properties.BooleanProperty;
 
-public class DLCreateTextBox extends DLEditBox {
+public class DLCreateTextBox extends DLRichTextEditBox {
+
+    public final BooleanProperty drawArrow = new BooleanProperty(false, false);
+
+    public DLCreateTextBox(int x, int y, int w, int h) {
+        super(x, y, w, h);
+        this.componentRenderer.set(CreateTextBoxRenderer.INSTANCE);
+    }
 
     protected boolean renderArrow;
     
-    public DLCreateTextBox(Font pFont, int pX, int pY, int pWidth, Component pMessage) {
-        super(pFont, pX + 5, pY + 5, pWidth - 10, 13, pMessage);
-        this.setBordered(false);
-    }
 
     public DLCreateTextBox setRenderArrow(boolean b) {
         this.renderArrow = b;
@@ -26,10 +29,7 @@ public class DLCreateTextBox extends DLEditBox {
     }
 
     @Override
-    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        Graphics graphics = new Graphics(guiGraphics, guiGraphics.pose());
-        CreateDynamicWidgets.renderTextBox(graphics, x() - 5, y() - 5, width() + 10);
+    public void renderMainLayer(DLGuiGraphics graphics, double mouseX, double mouseY, Rectangle renderBounds) {
         if (shouldRenderArrow()) CreateDynamicWidgets.renderTextBoxArrow(graphics, x() - 5, y() - 5);
-        super.renderWidget(graphics.graphics(), mouseX, mouseY, partialTick);
     }
 }
