2ea0f1d4de74b5138e82308c057c80cd41436639
MrJulsen <geisterdrache02@gmail.com>
2025-11-16 00:29:15 +0100

    Fixed random crashes with cursed solutions

--- common/src/main/java/de/mrjulsen/crn/CRNPlatformSpecific.java
+++ common/src/main/java/de/mrjulsen/crn/CRNPlatformSpecific.java
@@ -4,11 +4,14 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.block.entity.BlockEntity;
-
 import java.nio.file.Path;
 
 import com.simibubi.create.content.contraptions.Contraption;
 import com.simibubi.create.content.trains.station.GlobalStation;
+import com.tterrag.registrate.util.entry.RegistryEntry;
+
+import de.mrjulsen.crn.block.display.AdvancedDisplaySource;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -46,6 +49,11 @@ public abstract class CRNPlatformSpecific {
 
     @ExpectPlatform
     public static BlockEntity getClientContraptionBlockEntity(Contraption contraption, BlockPos localPos) {
+        throw new AssertionError();
+    }
+    
+    @ExpectPlatform
+    public static final RegistryEntry<AdvancedDisplaySource> registerDisplaySource() {
         throw new AssertionError();
     }
 }
--- common/src/main/java/de/mrjulsen/crn/registry/ModExtras.java
+++ common/src/main/java/de/mrjulsen/crn/registry/ModExtras.java
@@ -1,17 +1,18 @@
 package de.mrjulsen.crn.registry;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
+
+import de.mrjulsen.crn.CRNPlatformSpecific;
 import de.mrjulsen.crn.CreateRailwaysNavigator;
 import de.mrjulsen.crn.block.display.AdvancedDisplaySource;
 import de.mrjulsen.crn.block.display.AdvancedDisplayTarget;
 
 public class ModExtras {
-
-    public static final RegistryEntry<AdvancedDisplaySource> ADVANCED_DISPLAY_SOURCE = CreateRailwaysNavigator.REGISTRATE.displaySource("advanced_display", AdvancedDisplaySource::new)
-            .register();
+                
+    public static final RegistryEntry<AdvancedDisplaySource> ADVANCED_DISPLAY_SOURCE = CRNPlatformSpecific.registerDisplaySource();
             
     public static final RegistryEntry<AdvancedDisplayTarget> ADVANCED_DISPLAY_BOARD_TARGET = CreateRailwaysNavigator.REGISTRATE.displayTarget("advanced_display", AdvancedDisplayTarget::new)
-            .register();
+        .register();
 
     public static void init() {
     }
