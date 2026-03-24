567285d35e6e40baad00774066a4c276679a08ba
MrJulsen <geisterdrache02@gmail.com>
2026-03-25 00:18:59 +0100

    [fixed] Prioritized Destination Instruction does not work with wildcards

--- common/src/main/java/de/mrjulsen/crn/data/schedule/instruction/PrioritizedDestinationInstruction.java
+++ common/src/main/java/de/mrjulsen/crn/data/schedule/instruction/PrioritizedDestinationInstruction.java
@@ -18,7 +18,9 @@ import com.simibubi.create.foundation.gui.ModularGuiLineBuilder;
 import de.mrjulsen.crn.CreateRailwaysNavigator;
 import de.mrjulsen.crn.client.ClientWrapper;
 import de.mrjulsen.crn.data.schedule.INavigationExtension;
+import de.mrjulsen.crn.data.train.TrainUtils;
 import de.mrjulsen.crn.mixin.ScheduleRuntimeAccessor;
+import de.mrjulsen.crn.util.ModUtils;
 import de.mrjulsen.crn.util.PenaltyResult;
 import de.mrjulsen.crn.util.PenaltyResult.Category;
 import de.mrjulsen.crn.util.PenaltyResult.Type;
@@ -144,8 +146,8 @@ public class PrioritizedDestinationInstruction extends DestinationInstruction {
 		Train train = accessor.crn$getTrain();
 		List<String> filters = getFilters();
 		List<Pattern> patterns = filters.stream()
-				.map(Pattern::compile)
-				.collect(Collectors.toList());
+				.map(ModUtils::buildPattern)
+				.toList();
 		INavigationExtension ext = (INavigationExtension)train.navigation;
 
 		DiscoveredPath selectedDestination = null;
@@ -153,9 +155,7 @@ public class PrioritizedDestinationInstruction extends DestinationInstruction {
 		boolean anyMatch = false;
 
 
-		MapCache<DiscoveredPath, GlobalStation, GlobalStation> navigationCache = new MapCache<>((station) -> {
-			return train.navigation.findPathTo(station, Double.MAX_VALUE);
-		}, GlobalStation::hashCode);
+		MapCache<DiscoveredPath, GlobalStation, GlobalStation> navigationCache = new MapCache<>((station) -> train.navigation.findPathTo(station, Double.MAX_VALUE), GlobalStation::hashCode);
 
 		if (!train.hasForwardConductor() && !train.hasBackwardConductor()) {
 			train.status.missingConductor();
