f887f75858eb984c68cf095460a068c864efd2af
C1200 <40738690+C1200@users.noreply.github.com>
2026-05-11 14:47:40 +0100

    add option to prevent scale and pos reset on display link update

---
+++ common/src/main/java/de/mrjulsen/crn/block/display/properties/components/IRetainScaleAndPosSetting.java
@@ -0,0 +1,36 @@
+package de.mrjulsen.crn.block.display.properties.components;
+
+import de.mrjulsen.crn.CreateRailwaysNavigator;
+import de.mrjulsen.crn.block.display.properties.IDisplaySettings;
+import de.mrjulsen.crn.client.gui.widgets.modular.GuiBuilderContext;
+import de.mrjulsen.mcdragonlib.util.TextUtils;
+import net.minecraft.network.chat.MutableComponent;
+
+/**
+ * For data conversion: Indicates that this class adopts the original
+ * property {@code timeDisplay} from the Advanced Displays.
+ * If the class should adopt this property, this interface must be
+ * implemented or the value will not be converted!
+ */
+public interface IRetainScaleAndPosSetting {
+  String GUI_LINE_RETAIN_NAME = "retain_scale_pos";
+
+  boolean DEFAULT_RETAIN_SCALE_POS = false;
+  String NBT_RETAIN_SCALE_POS = "RetainScalePos";
+
+  MutableComponent txtRetain = TextUtils.translate("gui." + CreateRailwaysNavigator.MOD_ID + ".advanced_display_settings.retain_scale_pos");
+  MutableComponent txtRetainDescription = TextUtils.translate("gui." + CreateRailwaysNavigator.MOD_ID + ".advanced_display_settings.retain_scale_pos.description");
+
+  boolean shouldRetainScaleAndPos();
+  void setShouldRetainScaleAndPos(boolean v);
+
+  default void buildRetainGui(GuiBuilderContext context) {
+    GuiBuilderWrapper.buildRetainGui(this, context);
+  }
+
+  default void copyRetainSettings(IDisplaySettings oldSettings) {
+    if (oldSettings instanceof IRetainScaleAndPosSetting o) {
+      setShouldRetainScaleAndPos(o.shouldRetainScaleAndPos());
+    }
+  }
+}
