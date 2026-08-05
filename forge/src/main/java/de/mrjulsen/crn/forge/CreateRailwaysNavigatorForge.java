804b18398f3e23b2ff46546181cecf28172fc402
MrJulsen <geisterdrache02@gmail.com>
2026-08-05 16:29:52 +0200

    [fixed] Version check while joining a server is not working (Forge)

--- forge/src/main/java/de/mrjulsen/crn/forge/CreateRailwaysNavigatorForge.java
+++ forge/src/main/java/de/mrjulsen/crn/forge/CreateRailwaysNavigatorForge.java
@@ -1,7 +1,11 @@
 package de.mrjulsen.crn.forge;
 
+import java.util.Objects;
+
 import de.mrjulsen.crn.CreateRailwaysNavigator;
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,8 +13,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class CreateRailwaysNavigatorForge {
     public CreateRailwaysNavigatorForge() {
         EventBuses.registerModEventBus(CreateRailwaysNavigator.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        registerVersionCheck();
         CreateRailwaysNavigator.load();
         CreateRailwaysNavigator.REGISTRATE.registerEventListeners(FMLJavaModLoadingContext.get().getModEventBus());
         CreateRailwaysNavigator.init();
+    }
+
+    private static void registerVersionCheck() {
+        final String version = ModLoadingContext.get().getActiveContainer().getModInfo().getVersion().toString();
+        ModLoadingContext.get().registerExtensionPoint(
+            IExtensionPoint.DisplayTest.class,
+            () -> new IExtensionPoint.DisplayTest(
+                () -> version,
+                (remoteVersion, isFromServer) -> Objects.equals(version, remoteVersion)));
     }
 }
